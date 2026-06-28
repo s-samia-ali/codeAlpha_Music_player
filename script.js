95366ee5e85c7f49b84143d7c7806c7b49420f3d
s-samia-ali <syedasamiaali84@gmail.com>
2026-06-28 23:37:25 +0500

    Update image size

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ const songsList = [
         title: "Bewafaa",
         artist: "Imran Khan",
         src: "./media/Bewafa - Imran Khan.mp3", 
-        img: "./media/bewafa.jpg"
+        img: "./media/ewafaa.jpg"
     }
 ];
 
